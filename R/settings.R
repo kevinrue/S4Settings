d1d5b034d9aa48c58cb5b2ef33838a8c97e32099
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-04-17 13:29:44 +0100

    create class for incremental readonly settings

---
+++ R/settings.R
@@ -0,0 +1,36 @@
+#' IncrementalReadOnlySettings class
+#'
+#' @return An object of class `IncrementalReadOnlySettings`.
+#' @export
+#'
+#' @examples
+#' IncrementalReadOnlySettings(
+#'   a = 1,
+#'   b = "hello",
+#'   c = TRUE,
+#'   d = 5L
+#' )
+setClass("IncrementalReadOnlySettings",
+  contains = "list"
+)
+
+IncrementalReadOnlySettings <- function(...) {
+  slots_list <- list()
+
+  args_list <- list(...)
+  for (name in names(args_list)) {
+    if (name %in% names(slots_list)) {
+      stop(sprintf("Value %s already set. Cannot be overriden in class %s", dQuote(name), dQuote("IncrementalReadOnlySettings")))
+    }
+    slots_list[[name]] <- args_list[[name]]
+  }
+
+  new("IncrementalReadOnlySettings", slots_list)
+}
+
+setMethod("show", "IncrementalReadOnlySettings", function(object) {
+  cat("class:", class(object), "\n")
+  l <- object@.Data
+  names(l) <- names(object)
+  print(l)
+})
